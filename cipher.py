0b859a66f212be47b5d52d09c73b5f2e4984f0e6
Katie Cassady <katie@cassadyweb.com>
2023-11-08 07:55:58 -0500

    2nd attempt

--- cipher.py
+++ cipher.py
@@ -1,1 +1,35 @@
-# add your code here
+caesar_cipher = {
+    "a" : "f",
+    "b" : "g",
+    "c" : "h",
+    "d" : "i",
+    "e" : "j",
+    "f" : "k",
+    "g" : "l", 
+    "h" : "m", 
+    "i" : "n", 
+    "j" : "o", 
+    "k" : "p", 
+    "l" : "q", 
+    "m" : "r", 
+    "n" : "s", 
+    "o" : "t", 
+    "p" : "u", 
+    "q" : "v", 
+    "r" : "w", 
+    "s" : "x", 
+    "t" : "y", 
+    "u" : "z", 
+    "v":"a", 
+    "w":"b", 
+    "x":"c", 
+    "y":"d", 
+    "z":"e",
+}
+message = input("Please enter a sentence:")
+message = message.lower()
+characters = message.split()
+output = ""
+for character in message:
+    output += caesar_cipher.get(character, character)
+print(output)
